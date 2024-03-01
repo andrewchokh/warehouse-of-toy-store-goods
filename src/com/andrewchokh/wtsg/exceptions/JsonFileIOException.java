c6261b5d10fe4df57e71131dda1b07929c948985
andrewchokh <andrewchokh@gmail.com>
2024-03-01 11:46:37 +0200

    Add new json exception

---
+++ src/com/andrewchokh/wtsg/exceptions/JsonFileIOException.java
@@ -0,0 +1,10 @@
+package com.andrewchokh.wtsg.exceptions;
+
+import java.io.IOException;
+
+public class JsonFileIOException extends IOException {
+
+    public JsonFileIOException(String message) {
+        super(message);
+    }
+}
